c9880dd2db5bd184651b8b5700c4a82a91262403
Axel Rasmussen <axel.rasmussen1@gmail.com>
2017-02-05 00:00:29 -0800

    Add tests for result parsing.

---
+++ src/tests/result.rs
@@ -0,0 +1,56 @@
+use otp::Otp;
+use result::*;
+
+static TEST_API_KEY: &'static [u8] = &[0x7d, 0xdb, 0x1a, 0x7d, 0xfa, 0x9a, 0x7f, 0x8b, 0xeb, 0x73,
+                                       0x6a, 0xb7, 0x71, 0xdb];
+static TEST_OTP: &'static str = "vvvvvvcucrlcietctckflvnncdgckubflugerlnr";
+static TEST_NONCE: &'static str = "gPjfNhJFeeHZgfC9kKifggiWmIApziQ8XA4Vye1e";
+
+lazy_static! {
+    #[cfg_attr(rustfmt, rustfmt_skip)]
+    static ref RESULT_CONSTRUCTION_TEST_CASES: Vec<(Vec<u8>, bool)> = vec![
+        // Valid result.
+        (vec![
+            format!("otp={}", TEST_OTP),
+            format!("nonce={}", TEST_NONCE),
+            "h=waUYX2BayANQT0W3lpDB3EzhFRs=".to_owned(),
+            "t=2017-02-05T03:16:19Z0302".to_owned(),
+            "status=OK".to_owned(),
+        ].join("\n").into_bytes(), true),
+        // OTP mismatch.
+        (vec![
+            "otp=foo".to_owned(),
+            format!("nonce={}", TEST_NONCE),
+            "h=waUYX2BayANQT0W3lpDB3EzhFRs=".to_owned(),
+            "t=2017-02-05T03:16:19Z0302".to_owned(),
+            "status=OK".to_owned(),
+        ].join("\n").into_bytes(), false),
+        // Nonce mismatch.
+        (vec![
+            format!("otp={}", TEST_OTP),
+            "nonce=foo".to_owned(),
+            "h=waUYX2BayANQT0W3lpDB3EzhFRs=".to_owned(),
+            "t=2017-02-05T03:16:19Z0302".to_owned(),
+            "status=OK".to_owned(),
+        ].join("\n").into_bytes(), false),
+        // Signature mismatch.
+        (vec![
+            format!("otp={}", TEST_OTP),
+            format!("nonce={}", TEST_NONCE),
+            "h=waUYX2BayANQTTW3lpDB3EzhFRs=".to_owned(),
+            "t=2017-02-05T03:16:19Z0302".to_owned(),
+            "status=OK".to_owned(),
+        ].join("\n").into_bytes(), false),
+    ];
+}
+
+#[test]
+fn test_result_construction() {
+    for test_case in RESULT_CONSTRUCTION_TEST_CASES.iter() {
+        let result = VerificationResult::new(TEST_API_KEY,
+                                             &Otp::new(TEST_OTP).unwrap(),
+                                             TEST_NONCE,
+                                             test_case.0.clone());
+        assert_eq!(test_case.1, result.is_ok());
+    }
+}
