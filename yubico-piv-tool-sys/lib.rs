d206e69f4d70580f1d8bd27f5a955268286d5fc7
Axel Rasmussen <axel.rasmussen1@gmail.com>
2017-12-03 20:39:51 -0800

    Apply rustfmt to yubico-piv-tool-sys.

--- yubico-piv-tool-sys/lib.rs
+++ yubico-piv-tool-sys/lib.rs
@@ -167,7 +167,8 @@ lazy_static! {
         m
     };
 
-    static ref TO_SCARDERR_H_MAPPING: HashMap<SmartCardError, pcsc_sys::LONG> = FROM_SCARDERR_H_MAPPING.iter().map(|pair| (*pair.1, *pair.0)).collect();
+    static ref TO_SCARDERR_H_MAPPING: HashMap<SmartCardError, pcsc_sys::LONG> =
+        FROM_SCARDERR_H_MAPPING.iter().map(|pair| (*pair.1, *pair.0)).collect();
 }
 
 impl SmartCardError {
@@ -190,73 +191,149 @@ impl SmartCardError {
 impl std::error::Error for SmartCardError {
     fn description(&self) -> &str {
         match *self {
-            SmartCardError::BadSeek => "An error occurred in setting the smart card file object pointer.",
-            SmartCardError::BrokenPipe => "The client attempted a smart card operation in a remote session, such as a client session running on a terminal server, and the operating system in use does not support smart card redirection.",
-            SmartCardError::CacheItemNotFound => "The requested item could not be found in the cache.",
-            SmartCardError::CacheItemStale => "The requested cache item is too old and was deleted from the cache.",
-            SmartCardError::CacheItemTooBig => "The new cache item exceeds the maximum per-item size defined for the cache.",
+            SmartCardError::BadSeek => {
+                "An error occurred in setting the smart card file object pointer."
+            }
+            SmartCardError::BrokenPipe => concat!(
+                "The client attempted a smart card operation in a remote session, such as a ",
+                "client session running on a terminal server, and the operating system in use ",
+                "does not support smart card redirection.",
+            ),
+            SmartCardError::CacheItemNotFound => {
+                "The requested item could not be found in the cache."
+            }
+            SmartCardError::CacheItemStale => {
+                "The requested cache item is too old and was deleted from the cache."
+            }
+            SmartCardError::CacheItemTooBig => {
+                "The new cache item exceeds the maximum per-item size defined for the cache."
+            }
             SmartCardError::Cancelled => "The action was canceled by an SCardCancel request.",
             SmartCardError::CancelledByUser => "The action was canceled by the user.",
-            SmartCardError::CantDispose => "The system could not dispose of the media in the requested manner.",
+            SmartCardError::CantDispose => {
+                "The system could not dispose of the media in the requested manner."
+            }
             SmartCardError::CardNotAuthenticated => "No PIN was presented to the smart card.",
-            SmartCardError::CardUnsupported => "The smart card does not meet minimal requirements for support.",
-            SmartCardError::CertificateUnavailable => "The requested certificate could not be obtained.",
-            SmartCardError::ChvBlocked => "The card cannot be accessed because the maximum number of PIN entry attempts has been reached.",
-            SmartCardError::CommDataLost => "A communications error with the smart card has been detected.",
+            SmartCardError::CardUnsupported => {
+                "The smart card does not meet minimal requirements for support."
+            }
+            SmartCardError::CertificateUnavailable => {
+                "The requested certificate could not be obtained."
+            }
+            SmartCardError::ChvBlocked => {
+                "The card can't be accessed because the max number of PIN attempts was reached."
+            }
+            SmartCardError::CommDataLost => {
+                "A communications error with the smart card has been detected."
+            }
             SmartCardError::CommError => "An internal communications error has been detected.",
-            SmartCardError::DirNotFound => "The specified directory does not exist in the smart card.",
-            SmartCardError::DuplicateReader => "The reader driver did not produce a unique reader name.",
+            SmartCardError::DirNotFound => {
+                "The specified directory does not exist in the smart card."
+            }
+            SmartCardError::DuplicateReader => {
+                "The reader driver did not produce a unique reader name."
+            }
             SmartCardError::Eof => "The end of the smart card file has been reached.",
             SmartCardError::FileNotFound => "The specified file does not exist in the smart card.",
-            SmartCardError::IccCreateOrder => "The requested order of object creation is not supported.",
-            SmartCardError::IccInstallation => "No primary provider can be found for the smart card.",
-            SmartCardError::InsufficientBuffer => "The data buffer for returned data is too small for the returned data.",
+            SmartCardError::IccCreateOrder => {
+                "The requested order of object creation is not supported."
+            }
+            SmartCardError::IccInstallation => {
+                "No primary provider can be found for the smart card."
+            }
+            SmartCardError::InsufficientBuffer => {
+                "The data buffer for returned data is too small for the returned data."
+            }
             SmartCardError::InternalError => "An internal consistency check failed.",
-            SmartCardError::InvalidAtr => "An ATR string obtained from the registry is not a valid ATR string.",
+            SmartCardError::InvalidAtr => {
+                "An ATR string obtained from the registry is not a valid ATR string."
+            }
             SmartCardError::InvalidChv => "The supplied PIN is incorrect.",
             SmartCardError::InvalidHandle => "The supplied handle was not valid.",
-            SmartCardError::InvalidParameter => "One or more of the supplied parameters could not be properly interpreted.",
-            SmartCardError::InvalidTarget => "Registry startup information is missing or not valid.",
-            SmartCardError::InvalidValue => "One or more of the supplied parameter values could not be properly interpreted.",
+            SmartCardError::InvalidParameter => {
+                "One or more of the supplied parameters could not be properly interpreted."
+            }
+            SmartCardError::InvalidTarget => {
+                "Registry startup information is missing or not valid."
+            }
+            SmartCardError::InvalidValue => {
+                "One or more of the supplied parameter values could not be properly interpreted."
+            }
             SmartCardError::NoAccess => "Access is denied to the file.",
             SmartCardError::NoDir => "The supplied path does not represent a smart card directory.",
             SmartCardError::NoFile => "The supplied path does not represent a smart card file.",
-            SmartCardError::NoKeyContainer => "The requested key container does not exist on the smart card.",
+            SmartCardError::NoKeyContainer => {
+                "The requested key container does not exist on the smart card."
+            }
             SmartCardError::NoMemory => "Not enough memory available to complete this command.",
             SmartCardError::NoPinCache => "The smart card PIN cannot be cached.",
             SmartCardError::NoReadersAvailable => "No smart card reader is available.",
             SmartCardError::NoService => "The smart card resource manager is not running.",
-            SmartCardError::NoSmartCard => "The operation requires a smart card, but no smart card is currently in the device.",
+            SmartCardError::NoSmartCard => {
+                "The operation requires a smart card, but no card is currently in the device."
+            }
             SmartCardError::NoSuchCertificate => "The requested certificate does not exist.",
             SmartCardError::NotReady => "The reader or card is not ready to accept commands.",
-            SmartCardError::NotTransacted => "An attempt was made to end a nonexistent transaction.",
+            SmartCardError::NotTransacted => {
+                "An attempt was made to end a nonexistent transaction."
+            }
             SmartCardError::PciTooSmall => "The PCI receive buffer was too small.",
             SmartCardError::PinCacheExpired => "The smart card PIN cache has expired.",
-            SmartCardError::ProtoMismatch => "The requested protocols are incompatible with the protocol currently in use with the card.",
-            SmartCardError::ReaderUnavailable => "The specified reader is not currently available for use.",
-            SmartCardError::ReaderUnsupported => "The reader driver does not meet minimal requirements for support.",
+            SmartCardError::ProtoMismatch => {
+                "The requested protocols are incompatible with the protocol currently in use."
+            }
+            SmartCardError::ReaderUnavailable => {
+                "The specified reader is not currently available for use."
+            }
+            SmartCardError::ReaderUnsupported => {
+                "The reader driver does not meet minimal requirements for support."
+            }
             SmartCardError::ReadOnlyCard => "The smart card is read-only and cannot be written to.",
-            SmartCardError::RemovedCard => "The smart card has been removed, so further communication is not possible.",
+            SmartCardError::RemovedCard => {
+                "The smart card has been removed, so further communication is not possible."
+            }
             SmartCardError::ResetCard => "The smart card was reset.",
-            SmartCardError::SecurityViolation => "Access was denied because of a security violation.",
-            SmartCardError::ServerTooBusy => "The smart card resource manager is too busy to complete this operation.",
+            SmartCardError::SecurityViolation => {
+                "Access was denied because of a security violation."
+            }
+            SmartCardError::ServerTooBusy => {
+                "The smart card resource manager is too busy to complete this operation."
+            }
             SmartCardError::ServiceStopped => "The smart card resource manager has shut down.",
-            SmartCardError::SharingViolation => "The smart card cannot be accessed because of other outstanding connections.",
-            SmartCardError::Shutdown => "The operation has been aborted to allow the server application to exit.",
-            SmartCardError::SystemCancelled => "The action was canceled by the system, presumably to log off or shut down.",
+            SmartCardError::SharingViolation => {
+                "The smart card cannot be accessed because of other outstanding connections."
+            }
+            SmartCardError::Shutdown => {
+                "The operation has been aborted to allow the server application to exit."
+            }
+            SmartCardError::SystemCancelled => {
+                "The action was canceled by the system, presumably to log off or shut down."
+            }
             SmartCardError::Timeout => "The user-specified time-out value has expired.",
             SmartCardError::Unexpected => "An unexpected card error has occurred.",
             SmartCardError::UnknownCard => "The specified smart card name is not recognized.",
-            SmartCardError::UnknownError => "An internal error has been detected, but the source is unknown.",
+            SmartCardError::UnknownError => {
+                "An internal error has been detected, but the source is unknown."
+            }
             SmartCardError::UnknownReader => "The specified reader name is not recognized.",
             SmartCardError::UnknownResMng => "An unrecognized error code was returned.",
-            SmartCardError::UnpoweredCard => "Power has been removed from the smart card, so that further communication is not possible.",
+            SmartCardError::UnpoweredCard => {
+                "Power removed from the smart card, so further communication isn't possible."
+            }
             SmartCardError::UnresponsiveCard => "The smart card is not responding to a reset.",
-            SmartCardError::UnsupportedCard => "The reader cannot communicate with the card, due to ATR string configuration conflicts.",
-            SmartCardError::UnsupportedFeature => "This smart card does not support the requested feature.",
+            SmartCardError::UnsupportedCard => {
+                "The reader can't communicate with the card, due to ATR string config conflicts."
+            }
+            SmartCardError::UnsupportedFeature => {
+                "This smart card does not support the requested feature."
+            }
             SmartCardError::WaitedTooLong => "An internal consistency timer has expired.",
-            SmartCardError::WriteTooMany => "An attempt was made to write more data than would fit in the target object.",
-            SmartCardError::WrongChv => "The card cannot be accessed because the wrong PIN was presented.",
+            SmartCardError::WriteTooMany => {
+                "An attempt was made to write more data than would fit in the target object."
+            }
+            SmartCardError::WrongChv => {
+                "The card cannot be accessed because the wrong PIN was presented."
+            }
         }
     }
 }
@@ -324,7 +401,9 @@ impl ykpiv_state {
 
     pub fn disconnect(&mut self) {
         if self.card != 0 {
-            unsafe { pcsc_sys::SCardDisconnect(self.card, pcsc_sys::SCARD_RESET_CARD); }
+            unsafe {
+                pcsc_sys::SCardDisconnect(self.card, pcsc_sys::SCARD_RESET_CARD);
+            }
             self.card = 0;
         }
 
